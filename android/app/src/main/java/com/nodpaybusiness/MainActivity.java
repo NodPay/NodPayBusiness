3e48c6e004d2348f6443b2672a691ef7454d9bb1
Garry Laly <garrylaly@gmail.com>
2021-08-14 23:21:38 +0700

    Change package id and project name

--- android/app/src/main/java/com/nodpaybusiness/MainActivity.java
+++ android/app/src/main/java/com/nodpaybusiness/MainActivity.java
@@ -1,4 +1,4 @@
-package com.nodpaymobile;
+package com.nodpaybusiness;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "NodPayMobile";
+    return "NodPayBusiness";
   }
 }
--- ios/NodPayBusinessTests/NodPayBusinessTests.m
+++ ios/NodPayBusinessTests/NodPayBusinessTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface NodPayMobileTests : XCTestCase
+@interface NodPayBusinessTests : XCTestCase
 
 @end
 
-@implementation NodPayMobileTests
+@implementation NodPayBusinessTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
